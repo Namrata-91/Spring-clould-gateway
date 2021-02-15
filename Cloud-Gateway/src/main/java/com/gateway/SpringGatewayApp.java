497f7e14e2c042589b2c4d6412d3148b2df06061
NamrataCE <NamrataCE@192.168.0.107>
2021-02-15 17:29:53 +0900

    Spring cloud gateway microservice using Java config

---
+++ Cloud-Gateway/src/main/java/com/gateway/SpringGatewayApp.java
@@ -0,0 +1,12 @@
+package com.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewayApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewayApp.class, args);
+	}
+}
